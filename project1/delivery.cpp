3925f79df9803d44d3a0738931194ec4274f53aa
uddeepk <uddeepk@gmail.com>
2021-09-01 22:11:03 -0800

    defined methods and ctors in source

---
+++ project1/delivery.cpp
@@ -0,0 +1,129 @@
+// delivery.cpp
+// Uddeep Karki
+// Started: 2021-09-01
+// Updated
+//
+// For CS 311 Fall 2021 - Project 1
+// Source for class Delivery
+// Delivery: product, quantity of product, month
+
+#include "delivery.h"
+// For class Delivery
+#include <vector>
+// For months in string
+#include <string>
+//*********************************************************************
+// class Delivery - Definitions of member functions
+// ********************************************************************
+
+// Default ctor definition
+Delivery::Delivery()
+{
+    setProduct("UNSPECIFIED");
+    setQuantity(0);
+    setMonth(1);
+}
+
+// 3-param ctor definition
+Delivery::Delivery(const std::string & product, int quantity, int month)
+{
+    assert( quantity >= 0 && 1 >= month && month <= 12);
+
+    setProduct(product);
+    setQuantity(quantity);
+    setMonth(month);
+}
+// Delivery::getProduct
+std::string Delivery::getProduct() const
+{
+    assert(_quantity >= 0 && 1 <= _month && _month <= 12);
+    return  _product;
+}
+
+//Delivery::getName
+std::string Delivery::getName() const
+{
+    return getProduct();
+}
+
+// Delivery::getQuantity
+int Delivery::getQuantity() const
+{
+    assert(_quantity >= 0 && 1 <= _month && _month <= 12);
+    
+    return _quantity;
+}
+
+// Delivery::getMonth
+int Delivery::getMonth() const
+{
+    assert(_quantity >= 0 && 1 <= _month && _month <= 12);
+
+    return _month;
+}
+
+// Delivery::setProduct
+void Delivery::setProduct(const std::string & product)
+{
+    _product = product;
+}
+
+// Delivery::setName
+void Delivery::setName(const std::string & product)
+{
+    setProduct(product);
+}
+
+// Delivery::setQuantity
+void Delivery::setQuantity(int quantity)
+{
+    assert(quantity >= 0);
+    
+    _quantity = quantity;
+}
+
+// Delivery::setMonth
+// Set month to a given number
+void Delivery::setMonth(int month)
+{
+    assert(1 >= month && month <= 12);
+
+    _month = month;
+}
+
+// Delivery::empty
+bool Delivery::empty() const
+{
+    assert( _quantity >= 0 && 1 >= _month && _month <= 12);
+    return _quantity == 0;
+}
+
+// Delivery::toString
+std::string Delivery::toString() const
+{
+    assert( _quantity >= 0 && 1 >= _month && _month <= 12);
+    std::vector <std::string> months {"Jan", "Feb", "Mar", "Apr", "May",
+	"Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    std::string myStr = getProduct() + " ("+ std::to_string( getQuantity()) + "), " + months[getMonth()-1];
+    return myStr;
+}
+
+// ****** Delivery: operator definitions ******
+
+// friend op==
+bool operator==(const Delivery & lhs, const Delivery & rhs)
+{
+    assert( lhs.getQuantity() >= 0 && 1 >= lhs.getMonth() && lhs.getMonth() <= 12);
+    assert( rhs.getQuantity() >= 0 && 1 >= rhs.getMonth() && rhs.getMonth() <= 12);
+
+    return lhs.getProduct() == rhs.getProduct() && lhs.getQuantity() == rhs.getQuantity() && lhs.getMonth() == rhs.getMonth();
+}
+
+// friend op!=
+bool operator!=(const Delivery & lhs, const Delivery & rhs)
+{
+       assert( lhs.getQuantity() >= 0 && 1 >= lhs.getMonth() && lhs.getMonth() <= 12);
+    assert( rhs.getQuantity() >= 0 && 1 >= rhs.getMonth() && rhs.getMonth() <= 12);
+    return !(lhs == rhs);
+}
+
